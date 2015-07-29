9a938bb3e47d111f880422e992871e5544180a2a
adriaan-telcred <adriaan@telcred.com>
2015-07-29 15:11:45 +0200

    Update OnDiscoveredTagListener.java
    Added helpful method comment.

--- android/src/main/java/nordpol/OnDiscoveredTagListener.java
+++ android/src/main/java/nordpol/OnDiscoveredTagListener.java
@@ -3,5 +3,9 @@ package nordpol.android;
 import android.nfc.Tag;
 
 public interface OnDiscoveredTagListener {
+    /** Extract an IsoDep related object from the received tag and use it.
+	 * @see {@link AndroidCard#get(Tag)}
+	 * @param t
+	 */
     public void tagDiscovered(Tag t);
 }
