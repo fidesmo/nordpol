6f2dbe1aefde62c4e0e26c9a41adaf24d26e7448
Petter Arvidsson <petter.arvidsson@fidesmo.com>
2015-09-30 15:01:10 +0200

    Add APDU utilities
    - Add functions to work with hex strings
    - Add functions to generate SELECT APDUs

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ lazy val base = project.in(file("."))
   .aggregate(core, android)
 
 lazy val core = project
+  .settings(libraryDependencies +=
+    "org.scalatest" %% "scalatest" % "2.2.4" % "test")
 
 lazy val android = project
   .dependsOn(core)
---
+++ core/src/test/scala/nordpol/ApduSpec.scala
@@ -0,0 +1,60 @@
+package nordpol
+
+import org.scalatest._
+
+class ApduSpec extends WordSpec with Matchers {
+  "Apdu.encodeHex" should {
+    "encode one byte" in {
+      Apdu.encodeHex(1.toByte) shouldEqual "01"
+    }
+    "encode FF" in {
+      Apdu.encodeHex(0xFF.toByte) shouldEqual "FF"
+    }
+    "encode several bytes" in {
+      Apdu.encodeHex(Array[Byte](0xFF.toByte, 0x00.toByte, 0x11.toByte)) shouldEqual "FF0011"
+    }
+  }
+  "Apdu.decodeHex" should {
+    "decode one byte" in {
+      Apdu.decodeHex("01") shouldEqual Array[Byte](0x01.toByte)
+    }
+    "decode FF" in {
+      Apdu.decodeHex("FF") shouldEqual Array[Byte](0xFF.toByte)
+    }
+    "decode ff" in {
+      Apdu.decodeHex("ff") shouldEqual Array[Byte](0xFF.toByte)
+    }
+    "decode an array of bytes" in {
+      Apdu.decodeHex("FF0011") shouldEqual Array[Byte](0xFF.toByte, 0x00.toByte, 0x11.toByte)
+    }
+    "refuse to decode uneven strings" in {
+      intercept[IllegalArgumentException] {
+        Apdu.decodeHex("123")
+      }
+    }
+  }
+  "Apdu.select" should {
+    "generate command for A000000151000000" in {
+      Apdu.select("A000000151000000") shouldEqual Apdu.decodeHex("00A4040008A000000151000000")
+    }
+    "generate command for appId 12345678 and suffix 01" in {
+      Apdu.select("12345678", "01") shouldEqual Apdu.decodeHex("00A404000BA000000617001234567801")
+    }
+  }
+  "Apdu.statusBytes" should {
+    "return status from empty response" in {
+      Apdu.statusBytes(Array[Byte](0x90.toByte, 0x00.toByte)) shouldEqual Array[Byte](0x90.toByte, 0x00.toByte)
+    }
+    "return status" in {
+      Apdu.statusBytes(Array[Byte](0x01.toByte, 0x90.toByte, 0x00.toByte)) shouldEqual Array[Byte](0x90.toByte, 0x00.toByte)
+    }
+  }
+  "Apdu.responseData" should {
+    "return data from empty response" in {
+      Apdu.responseData(Array[Byte](0x90.toByte, 0x00.toByte)) shouldEqual Array.empty[Byte]
+    }
+    "return data" in {
+      Apdu.responseData(Array[Byte](0x01.toByte, 0x90.toByte, 0x00.toByte)) shouldEqual Array[Byte](0x01.toByte)
+    }
+  }
+}
